6645b99de429695aaab281ab5313a96dbef2d504
Pablo Mariasch <103390999+Arteok@users.noreply.github.com>
2022-11-11 15:55:47 -0300

    11-11-2022

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//json_encode()
+$objeto([]);
+
+/*$objeto->nombre = "Juan";
+$objeto-> edad = 23;
+$objeto-> pais = "Venezuela";
+
+$miJSON = json_encode($objeto);*/
+$data = '{
+    "name": "John Doe",
+    "occupation": "gardener"
+}';
+var_dump($data);
+?>
